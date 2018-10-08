a4e40d31ecb651d09a76ac1de51efb67b6fae1bd
Matt Gardner <matthewlgardner@gmail.com>
2018-10-07 20:13:21 -0600

    grab title URLs and put in array

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 var myApp = angular.module('idahoStatutes', [])
 
-myApp.controller('mainController', ['$scope', function($scope) {
+myApp.controller('mainController', [
+    '$scope',
+    '$http',
+    function($scope, $http) {
 
-$scope.name = 'Matt'
+        $http({method: 'GET', url: 'https://legislature.idaho.gov/statutesrules/idstat'}).then(function(response) {
+            // console.log('response = ', response.data);
+            var response = response.data
+            var titleRegex = /\/statutesrules\/idstat\/Title[0-9]+/g
 
-}])
+            var titleUrlArr = response.match(titleRegex)
+
+//recursive function??
+
+            for (var i = 0; i < titleUrlArr.length; i++) {
+                // console.log(titleUrlArr[i])
+                var titleLink = `https://legislature.idaho.gov${titleUrlArr[i]}`
+                console.log(titleLink);
+            }
+
+        })
+
+    }
+])
